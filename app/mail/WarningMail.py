9ee0052979c5177e6e8e0cdf07011ca1db7fce87
tetsu <nilssteinhauer4@gmail.com>
2022-12-09 14:29:44 +0100

    #15: add email class.

---
+++ app/mail/WarningMail.py
@@ -0,0 +1,27 @@
+import smtplib
+import configparser
+
+
+class WarningMail:
+    @staticmethod
+    def send_mail(log, email):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        print(config.sections())
+        port = 587  # For starttls
+        smtp_server = config['sendermail']['smtp']
+        sender_email = config['sendermail']['email']
+        password = config['sendermail']['passwd']
+        message = open('mail.txt', 'r').read().replace('$LOG', log)
+        message = '\n' + message
+        server = smtplib.SMTP(host=smtp_server, port=port)
+        server.ehlo()
+        server.starttls()
+        server.login(sender_email, password)
+        server.ehlo()
+        header = 'To:' + email + '\n' + 'From: ' + sender_email + '\n' + 'Subject:WARNING \n ' \
+                                                                         '\nContent-Type: text/plain; charset=UTF-8 \n'
+        msg = header + message
+
+        server.sendmail(from_addr=sender_email, to_addrs=email, msg=msg)
+        server.quit()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from webserver.server import create_app
 
+
 class Main:
     create_app().run('0.0.0.0', 5000)
+
 
 if __name__ == "__main__":
     Main()
